6460779ee1b2a17eb4c840f903464992937d298a
Paul Lasry-Robin <paul.lasryrobin@gmail.com>
2024-04-23 18:54:15 +0200

    Put the url of the database on __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,21 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from .config import config
+# from .config import config
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 # CONFIF_MODE = os.getenv("CONFIG_MODE")
-CONFIG_MODE = 'development'
+# CONFIG_MODE = 'development'
 
 app = Flask(__name__, template_folder='templates')
     
-app.config.from_object(config[CONFIG_MODE])
+#app.config.from_object(config[CONFIG_MODE])
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 db.init_app(app)
 migrate.init_app(app, db)
