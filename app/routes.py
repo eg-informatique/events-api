d0bbd250285626fb3788d86d308c139d0837f458
Paul Lasry-Robin <paul.lasryrobin@gmail.com>
2024-08-13 14:51:34 +0200

    Bug fix

--- app/routes.py
+++ app/routes.py
@@ -21,7 +21,7 @@ def index():
 def get_events():
     args = request.args
     page = args.get('page') if 'page' in args else 0
-    pages = int(page)*15
+    pages = int(page)*6
     events_query = Event.query.order_by(Event.start_datetime.asc())
     search_query = args.get("search", "").strip()
     if search_query and search_query != "":
