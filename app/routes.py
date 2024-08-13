b091827f765d122d849b386b8b34b26df790152a
Paul Lasry-Robin <paul.lasryrobin@gmail.com>
2024-08-13 14:40:14 +0200

    Test

--- app/routes.py
+++ app/routes.py
@@ -24,6 +24,7 @@ def get_events():
     pages = int(page-1)*15
     events_query = Event.query.order_by(Event.start_datetime.asc())
     search_query = args.get("search", "").strip()
+    return search_query
     if search_query and search_query != "":
         events_query = events_query.filter(
             or_(
