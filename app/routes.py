9e4066a415ad0e1c929b8c91e013ffdf4db08b53
Paul <paul.lasryrobin@gmail.com>
2025-01-02 18:51:05 +0100

    Fix filename format for registered PDF download

--- app/routes.py
+++ app/routes.py
@@ -481,4 +481,4 @@ def get_registred_pdf():
     pdf_path = f"/tmp/{event.title.replace(' ', '_')}_registered_list.pdf"
     pdf.output(pdf_path)
 
-    return send_file(pdf_path, as_attachment=True, download_name=f"{event.title}_registered_list.pdf")
+    return send_file(pdf_path, as_attachment=True, download_name=f"{event.title}-registered-list.pdf")
